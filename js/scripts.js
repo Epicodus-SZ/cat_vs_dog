a0f557800c59dd2d7596b21371eb4608bb916bc4
Anna Timofeeva and Steve Zaske <anna@>
2017-05-31 13:24:33 -0700

    with fix for making pictures removing

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,22 @@ $(document).ready(function() {
   $("button#cat").click(function() {
     $("ul#cat").prepend("<li>Meow (purrrrr)</li>");
     $("ul#cat").children("li").first().click(function() {
-      $("img#catpic").toggle();
+      $("h2#catColumn").before("<img id='catpic' class='begone' src='img/cat.png'>");
+      $("img.begone").click(function() {
+        $(this).remove();
+      });
     });
-
   });
 
   $("button#dog").click(function() {
     $("ul#dog").prepend("<li>Woof! Grrrrrrr</li>");
     $("ul#dog").children("li").first().click(function() {
-      $("img#dogpic").toggle();
+      $("h2#dogColumn").before("<img id='dogpic' class='begone' src='img/dog.png'>");
+      $("img.begone").click(function() {
+        $(this).remove();
+      });
     });
-
-
   });
+
+
 });
